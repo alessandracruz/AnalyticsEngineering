d385c0d9241deb3b809894803ddd41a7f32a7edc
Felipe Zanardo <felipebueno6@hotmail.com>
2024-11-10 11:12:55 -0300

    (fix) correção de erros na camada prata e implementação de uma macro para limpeza de textos

--- airbnb_project/models/silver/airbnb_reviews_silver.sql
+++ airbnb_project/models/silver/airbnb_reviews_silver.sql
@@ -7,5 +7,5 @@ SELECT
     TO_DATE(date, '%Y-%m-%d') AS date,
     reviewer_id,
     reviewer_name,
-    COALESCE(REPLACE(REGEXP_REPLACE(comments, '[^\w\s]+', ''), '', null), 'no_comments') AS comments
+    {{ clean_text('comments') }} AS comments
 FROM {{ ref('airbnb_reviews_bronze') }}
